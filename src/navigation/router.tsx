80fb36ff1352eac23f93b304e56344d09653afb5
Thiago349 <thiagoreis2002@gmail.com>
2024-09-24 08:05:37 -0300

    feat: adding trailing slashs removal logic

--- src/navigation/router.tsx
+++ src/navigation/router.tsx
@@ -1,5 +1,7 @@
-import { Navigate, BrowserRouter, Route, Routes, useLocation, Outlet } from 'react-router-dom'
-import { useAppSelector } from "src/redux/store";
+import { useEffect } from 'react';
+
+import { Navigate, BrowserRouter, Route, Routes, useLocation, useNavigate, Outlet } from 'react-router-dom'
+import { useAppSelector, } from "src/redux/store";
 
 import { Login } from 'src/pages';
 import { MenuLayout } from 'src/components/MenuLayout/MenuLayout';
@@ -7,6 +9,15 @@ import { MenuLayout } from 'src/components/MenuLayout/MenuLayout';
 import { paths, PathProps } from './paths';
 
 export const Router = () => {
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (location.pathname !== '/' && location.pathname.endsWith('/')) {
+            navigate(location.pathname.slice(0, -1), { replace: true });
+        }
+    }, [location, navigate]);
+
     const status = useAppSelector(state => state?.authentication?.status)
     function PrivateRoutes() {
         const { pathname: from } = useLocation();
