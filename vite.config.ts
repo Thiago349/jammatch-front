707a2db58fe64c22748549fef04b33dd7b93370e
Thiago349 <thiagoreis2002@gmail.com>
2024-09-24 08:59:27 -0300

    feat: vite build rollup config

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,11 @@ import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: '/index.html'
+    }
+  },
   resolve: {
     alias: {
       'src': path.resolve(__dirname, './src')
